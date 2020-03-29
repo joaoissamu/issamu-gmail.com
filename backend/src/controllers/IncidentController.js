17e1b8245b872d9f53cab94ab241f268fb006e26
João Issamu <issamu@joiss.com.br>
2020-03-29 00:51:24 -0300

    Backend da aplicação

---
+++ backend/src/controllers/IncidentController.js
@@ -0,0 +1,62 @@
+const connection = require('../database/connection');
+
+module.exports = {
+    async index(request, response) {
+        const { page = 1 } = req.query;
+
+        const [count] = await connection("incidents").count();
+
+        const incidents = await connection("incidents")
+        .join("ongs", "ongs.id", "=", "incidents.ong_id")
+        .limit(5)
+        .offset((page - 1) * 5)
+        .select([
+            "incidents.*",
+            "ongs.name",
+            "ongs.whatsapp",
+            "ongs.email",
+            "ongs.city",
+            "ongs.uf"
+        ]);
+
+        res.header("X-Total-Count", count["count(*)"]);
+
+        return res.json(incidents);
+    },
+
+    async create(request, response) {
+        const { title, description, value } = request.body;
+        const ong_id = request.headers.authorization; 
+        const [id] = await connection('incidents').insert({
+            title,  
+            description, 
+            value,
+            ong_id
+        });
+
+        return response.json({ id });
+    },
+
+    async delete(request, response) {
+        const {id} = request.params; 
+        const ong_id = request.headers.authorization; 
+
+        const incident = await connection('incidents')
+            .where('id', id)
+            .select('ong_id')
+            .first();
+
+        try {
+            if (incident.ong_id != ong_id) {
+                return response.status(401).json({ error: 'Operação não permitida.'});
+            }
+
+            await connection('incidents').where('id', id).delete();
+
+            return response.status(204).send();
+        } catch (e) {
+            console.log(e);
+            return response.status(500).json({ error: 'Erro ao realizar a operação'});
+        }
+    }
+}
